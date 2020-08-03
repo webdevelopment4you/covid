efaf9345c6852dd5ca7d638a77ba91c8c9cc6b60
webdevelopment4you <65403221+webdevelopment4you@users.noreply.github.com>
2020-08-03 17:57:15 +0530

    Add files via upload

---
+++ javascript/covid.js
@@ -0,0 +1,121 @@
+fetch("https://api.covid19india.org/data.json").then( (response) => {
+
+    return response.json();
+
+}).then( (data) => {
+
+    let counter = -1;
+    let tableHead = document.getElementById("tableHead");
+    let tableBody = document.getElementById("tableBody");
+
+    let string =
+    `
+    <tr>
+    <th scope="col">  S.No.</th>
+    <th scope="col">State</th>
+    <th scope="col">Active</th>
+    <th scope="col">Confirmed</th>
+    </tr>
+
+    `
+    tableHead.innerHTML = string
+
+    let tableContent="";
+
+    for (const cases of data.statewise) {
+
+        if(counter != -1)
+        {
+            tableContent += 
+            `
+                <tr>
+                <th scope="row">${counter+1}</th>
+                <td><a id=${cases.state} onclick = "getDistrictData(this.id)">${cases.state}</a></td>
+                <td>${cases.active}</td>
+                <td>${cases.confirmed}</td>
+                </tr>
+        `
+        }
+    counter++;
+
+    }
+
+    tableBody.innerHTML = tableContent;
+
+    
+    
+})
+
+
+function getDistrictData(id){
+
+    let stateName = document.getElementById(id).innerText;
+    
+    // console.log(stateName);
+
+        fetch("https://api.covid19india.org/state_district_wise.json").then( (response) => {
+    
+            return response.json();
+    
+        }).then( (data) => {
+
+            
+            let counter = 0;
+            let tableHead = document.getElementById("tableHead");
+            let tableBody = document.getElementById("tableBody");
+    
+            let string =
+            `
+            <tr>
+            <th scope="col">  S.No.</th>
+            <th scope="col">District</th>
+            <th scope="col">Active</th>
+            <th scope="col">Confirmed</th>
+            </tr>
+    
+          `
+          tableHead.innerHTML = string
+    
+          let tableContent="";
+    
+                for (const districtData in data[stateName]) {
+    
+                    // console.log(data["Rajasthan"][districtData]);
+    
+                    for (const place in data[stateName][districtData]) {
+    
+                        // console.log(place);
+    
+                        // console.log(data[stateName][districtData][place]);
+
+                        if(data[stateName][districtData][place].active != undefined)
+                        {
+                                tableContent += 
+                                `
+                                    <tr>
+                                    <th scope="row">${counter+1}</th>
+                                    <td>${place}</td>
+                                    <td>${data[stateName][districtData][place].active}</td>
+                                    <td>${data[stateName][districtData][place].confirmed}</td>
+                                    </tr>
+                            `
+                        }
+                        counter++;
+                
+    
+                    }
+    
+                }
+
+                tableBody.innerHTML = tableContent;
+
+                let back = document.getElementById("back");
+                back.innerHTML = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M5.854 4.646a.5.5 0 0 1 0 .708L3.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z"/>
+                <path fill-rule="evenodd" d="M2.5 8a.5.5 0 0 1 .5-.5h10.5a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+              </svg><a onclick="location.reload()">back</a>`
+    
+    })
+
+}
+
