c459ff9c342440c418e8e6ab19601e812cc5485b
webdevelopment4you <65403221+webdevelopment4you@users.noreply.github.com>
2020-08-04 17:59:31 +0530

    Add files via upload

--- javascript/covid.js
+++ javascript/covid.js
@@ -1,4 +1,10 @@
 let info = document.getElementById("detailsInfo");
+let casesContainer = document.getElementById("casesContainer");
+let detailsContainer = document.getElementById("detailsContainer");
+let backButtonFunction = true;
+
+detailsContainer.style.display = "none";
+
 
 fetch("https://api.covid19india.org/data.json").then( (response) => {
 
@@ -6,6 +12,7 @@ fetch("https://api.covid19india.org/data.json").then( (response) => {
 
 }).then( (data) => {
 
+    console.log(data);
     let counter = -1;
     let tableHead = document.getElementById("tableHead");
     let tableBody = document.getElementById("tableBody");
@@ -51,12 +58,21 @@ fetch("https://api.covid19india.org/data.json").then( (response) => {
 
 
 function getDistrictData(id){
-
-    info.style.display = "none";
-
-    let stateName = document.getElementById(id).innerText;
     
-    // console.log(stateName);
+    info.style.display = "none";
+    casesContainer.style.display = "block";
+    detailsContainer.style.display = "none";
+
+    if(backButtonFunction == true)
+    {
+        stateName = document.getElementById(id).innerText;
+    }
+
+    else
+    {
+        stateName = id.getAttribute("id");
+
+    }
 
         fetch("https://api.covid19india.org/state_district_wise.json").then( (response) => {
     
@@ -119,8 +135,27 @@ function getDistrictData(id){
                 <path fill-rule="evenodd" d="M5.854 4.646a.5.5 0 0 1 0 .708L3.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z"/>
                 <path fill-rule="evenodd" d="M2.5 8a.5.5 0 0 1 .5-.5h10.5a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
               </svg><a onclick="location.reload()">back</a>`
+
+              let more = document.getElementById("more");
+              more.innerHTML = `<a id=${stateName} onclick = "getIndividualStateData(this.id)">Click here to know more</a>`;
     
     })
+
+}
+
+function getIndividualStateData(stateId)
+{
+    detailsContainer.style.display = "block";
+    casesContainer.style.display = "none";
+    backButtonFunction = false;
+
+    
+    let back2 = document.getElementById("back2");
+    back2.innerHTML = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" d="M5.854 4.646a.5.5 0 0 1 0 .708L3.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z"/>
+    <path fill-rule="evenodd" d="M2.5 8a.5.5 0 0 1 .5-.5h10.5a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+    </svg><a onclick="getDistrictData(${stateId})">back</a>`
+    
 
 }
 
